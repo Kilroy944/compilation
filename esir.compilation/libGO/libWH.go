795d5793a829ced18f9aba72aacb58f8b87fbd4b
VLion <valentin.lion@laposte.net>
2018-01-22 10:22:20 +0100

    Modif symbol NIL

--- esir.compilation/libGO/libWH.go
+++ esir.compilation/libGO/libWH.go
@@ -7,49 +7,43 @@ type Tree struct {
 }
 func TreeToNumber ( t *Tree)(int){
 	var counter = 0
-	for t != nil && t.Symbol!="nil" {
-    	counter++
-    	t = t.Right
+	for t.Symbol!="nil" {
+    		counter++
+    		t = t.Right
 	}
 	return counter
 }
 func TreeToRange (t *Tree)([] *Tree){
 	var tab = [](*Tree){}
-	for t != nil {
-    	tab = append(tab,t.Right)
-    	t = t.Right
+	for t.Symbol!="nil" {
+    		tab = append(tab,t.Right)
+    		t = t.Right
 	}
 	return tab
 }
 func NumberToTree(nb int)(*Tree){
 	var t *Tree	
 	for i := 0; i < nb; i++ {
-		t = Cons(nil,t)
+		t = Cons(&(Tree{"nil",nil,nil}),t)
 	}
 	return t
 }
 func Tl(t *Tree)(*Tree){
-
-	if t!=nil {
+	if t.Symbol!="nil" {
 		return t.Right
 	}
-	return nil
+	return &(Tree{"nil",nil,nil})
 }
 func Hd(t *Tree)(*Tree){
-	if t!=nil {
+	if t.Symbol!="nil" {
 		return t.Left
 	}
-	return nil
+	return &(Tree{"nil",nil,nil})
 }
 func Cons(g *Tree,d *Tree)(*Tree){
 	return &(Tree{"",g,d})
 }
 func IsTrue (t *Tree)(bool){
-	
-	if t==nil {
-		return false
-	}
-
 	if t.Symbol=="nil" {
 		return false
 	}
@@ -60,25 +54,25 @@ func Op_or (t1,t2 *Tree)(*Tree){
     if(IsTrue(t1) || IsTrue(t2)){
    	 return &(Tree{"",nil,nil})
     }
-    return nil
+    return &(Tree{"nil",nil,nil})
 }
 func Op_eg (t1,t2 *Tree)(*Tree){
 	
-	if (t1==nil && t2==nil) {
+	if (t1.Symbol==nil && t2.Symbol==nil) {
 		return &(Tree{"",nil,nil})	
-	}else if (t1!=nil && t2!=nil) {
-	   if Op_eg(t1.Right,t2.Right)!= nil && Op_eg(t1.Left,t2.Left) !=nil {
+	}else if (t1.Symbol!=nil && t2.Symbol!=nil) {
+	   if Op_eg(t1.Right,t2.Right)!= &(Tree{"nil",nil,nil}) && Op_eg(t1.Left,t2.Left) != &(Tree{"nil",nil,nil}) {
 		return &(Tree{"",nil,nil})	
 	   }else{
-		return nil
+		return &(Tree{"nil",nil,nil})
 	   }
 	}else{
-	  return nil
+	  return &(Tree{"nil",nil,nil})
 	}
 }
 func Op_not (t *Tree)(*Tree){
     if IsTrue(t){
-   	 return nil
+   	 return &(Tree{"nil",nil,nil})
     }
     return &(Tree{"",nil,nil})
 }
@@ -86,5 +80,5 @@ func Op_and (t1,t2 *Tree)(*Tree){
     if(IsTrue(t1) && IsTrue(t2)){
    	 return &(Tree{"",nil,nil})
     }
-    return nil
+    return &(Tree{"nil",nil,nil})
 }
